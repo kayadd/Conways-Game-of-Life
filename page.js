d469cea55849d171323ec1da793ffb8c082ce293
kayadd <blue26lol@gmail.com>
2023-08-02 03:17:03 +0200

    Refactored the code to use canvas to make drawing easier and tried to add onClick, but it still contains bugs.

--- page.js
+++ page.js
@@ -4,16 +4,34 @@
 let state = 0;
 
 // Initialisiert die neue Squareliste.
+let xSquares = [];
 let squares = [];
 
-for(let i = 0; i < , i++) {
-    var sqdiv = document.createElement("div");
-    sqdiv.className = "square";
-    sqdiv.id = i;
-    sqdiv.style = "margin-top: -742px; margin-left: 320px;"
-    document.body.appendChild(sqdiv);
+// Initialisiert jede Zelle als Zelle mit weißem Hintergrund.
+for(let rows = 0; rows < 54; rows++) {
+    xSquares.push(0);
+}
 
+for(let columns = 0; columns < 38; columns++) {
+    squares.push(xSquares);
+}
 
+function graph(){
+    const canvas = document.getElementById("box");
+    const ctx = canvas.getContext("2d");
+
+    for(let rows = 0; rows < 54 ; rows++) {
+        for(let columns = 0; columns < 38 ; columns++) {
+            ctx.fillStyle = "white";
+            if (squares[columns][rows] == 1)
+            {
+                ctx.fillStyle = "black";
+            }
+
+            ctx.fillRect(20*rows, 20*columns, 20, 20);
+
+        }
+    }
 }
 
 // Wenn auf den Bildschirm geklickt wird, wird diese Funktion aufgerufen.
@@ -24,21 +42,21 @@ function OnClick(event) {
     let yCourser = event.clientY;
 
     // Wenn der Cursor innerhalb dieser Box ist:
-    if ((300 <= xCourser && xCourser <= 1380) && (28 <= yCourser && yCourser <= 768 ))
-    {      // Formatiert die neuen Koordinaten des Quadrates.
-           cX = xCourser - (xCourser % 20);
-           cY = yCourser - (yCourser % 20);
+    if ((0 <= xCourser && xCourser <= 1080) && (0 <= yCourser && yCourser <= 760 ))
+    {   // Formatiert die neuen Koordinaten des Quadrates.
+        idx = (xCourser - (xCourser % 20))/(20);
+        idy = (yCourser - (yCourser % 20))/(20);
 
-           idx =(cX-300)/(20) ;
-           idy = (cY-742)/(20);
-
-           // Erschafft einen neuen div.
-           var sqdiv = document.createElement("div");
-           sqdiv.className = "square";
-           sqdiv.id = idx.toString()+","+idy.toString();
-           sqdiv.style = "margin-top: -742px; margin-left: 320px;"
-           document.body.appendChild(sqdiv);
+        if (squares[idx][idy] == 0)
+        {
+            squares[idx][idy] = 1;
+        } else
+        {
+            squares[idx][idy] = 0;
+        }
     }
+
+    graph();
 }
 
 document.addEventListener("click", OnClick);
